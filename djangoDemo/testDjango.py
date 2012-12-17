df8d8284cd8dd43cf40db6a5090dce58325dbffc
wenpingliu <wenpingliu@sohu-inc.com>
2012-12-17 21:54:59 +0800

    Revert "init"
    This reverts commit ed43a8621b3b4d25dbf2249204ea9ec3f87ecb16.

--- djangoDemo/testDjango.py
+++
@@ -1,7 +0,0 @@
-'''
-Created on 2012-12-17
-
-@author: wenpingliu
-'''
-import django
-print django.VERSION;
--- djangoDemo/testDjangoDB.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on 2012-12-17
-
-@author: wenpingliu
-'''
-from django.db import connection
-cursor = connection.cursor()
-print cursor
